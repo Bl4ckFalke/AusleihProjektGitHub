d53d325e757ff2b22d7bdaf22c8dd246e943ee49
myliusn <myliusn@bswiesaude.onmicrosoft.com>
2025-07-15 10:59:10 +0200

    VerwaltungFenster WPF erstellt (UI noch nicht fertig)

---
+++ AusleihProjektGitHub/UI/VerwaltungFenster.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AusleihProjektGitHub.UI
+{
+    /// <summary>
+    /// Interaktionslogik für VerwaltungFenster.xaml
+    /// </summary>
+    public partial class VerwaltungFenster : Window
+    {
+        public VerwaltungFenster()
+        {
+            InitializeComponent();
+        }
+    }
+}
