4257d7b5045f4ec7d64d267747381ad6d2fce809
wipauerj <wipauerj@bswiesaude.onmicrosoft.com>
2025-07-17 09:33:21 +0200

    kleie änderung an fenster

---
+++ AusleihProjektGitHub/UI/HauptFenster.xaml.cs
@@ -0,0 +1,51 @@
+﻿using AusleihProjektGitHub.Fachklassen;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AusleihProjektGitHub;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class HauptFenster : Window
+{
+    public HauptFenster()
+    {
+        InitializeComponent();
+        
+    }
+
+    private void bttn_erstellen(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void bttn_verwalten(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void suche_Name(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void RadioButton_Checked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void CheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+}
--- AusleihProjektGitHub/UI/MainWindow.xaml.cs
+++ AusleihProjektGitHub/UI/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using AusleihProjektGitHub.Fachklassen;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -7,45 +10,33 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AusleihProjektGitHub;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace AusleihProjektGitHub.UI
 {
-    public MainWindow()
+    /// <summary>
+    /// Interaktionslogik für LoginFenster.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UserLogin(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PwdLogin(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
-
-    private void bttn_erstellen(object sender, RoutedEventArgs e)
-    {
-
-    }
-
-    private void bttn_verwalten(object sender, RoutedEventArgs e)
-    {
-
-    }
-
-    private void suche_Name(object sender, TextChangedEventArgs e)
-    {
-
-    }
-
-    private void RadioButton_Checked(object sender, RoutedEventArgs e)
-    {
-
-    }
-
-    private void CheckBox_Checked(object sender, RoutedEventArgs e)
-    {
-
-    }
-
 }
